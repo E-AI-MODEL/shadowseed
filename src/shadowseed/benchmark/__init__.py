aab230823b4a87bbfb24a4d8832aeeaaad1029ab
EAI <vis@emmauscollege.nl>
2026-05-02 18:42:28 +0200

    Add benchmark preparation helpers

---
+++ src/shadowseed/benchmark/__init__.py
@@ -0,0 +1,5 @@
+"""Benchmark helpers for Shadow Seed Learning."""
+
+from .absencebench import AbsenceBenchPreparation, build_preparation_record
+
+__all__ = ["AbsenceBenchPreparation", "build_preparation_record"]
